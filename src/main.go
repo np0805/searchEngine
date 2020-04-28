5104dbce648ce085c534742d9d8acfaf0758289e
np0805 <nickypratama17@yahoo.com>
2020-04-28 19:28:58 +0700

    restructure file position and add more explanation to crawler.go functions

---
+++ src/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"./crawler"
+)
+
+func main() {
+	const baseURL = "https://www.cse.ust.hk/"
+	fmt.Println(time.Now())
+
+	// pages := make([]*Page, 0)
+	// basePage := Page{baseURL, "", "", "", make([]string, 0), nil, make([]string, 0)}
+	// pages = append(pages, &basePage)
+	// WriteIndexed(&pages)
+
+	pagesMap := make(map[string]*crawler.Page)
+	basePage := crawler.Page{
+		URL:          baseURL,
+		Title:        "",
+		LastModified: "",
+		PageSize:     "",
+		Keywords:     make([]string, 0),
+		ParentURL:    nil,
+		ChildrenURL:  make([]string, 0)}
+
+	basePage.ExtractTitle()
+	basePage.ExtractLastModified()
+	basePage.ExtractWords()
+	basePage.ExtractSize()
+	basePage.ExtractLinks()
+
+	pagesMap[baseURL] = &basePage
+
+	basePage.WriteIndexed(&pagesMap)
+
+	// // contoh cara ngeindex dari map
+	// another := pagesMap["http://epublish.ust.hk/cgi-bin/eng/story.php?id=96&catid=97&keycode=88b7aae0ae45ddb0e6e000ee2682721a&token=17b43a00aeb0f8f8f08df16ae664909f"]
+	// fmt.Println(another.GetParentURL())
+	// fmt.Println(pagesMap["http://epublish.ust.hk/cgi-bin/eng/story.php?id=96&catid=97&keycode=88b7aae0ae45ddb0e6e000ee2682721a&token=17b43a00aeb0f8f8f08df16ae664909f"])
+	// fmt.Println(len(pagesMap))
+
+	fmt.Println(time.Now())
+
+}
