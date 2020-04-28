5ce5c2d69fc8014c21ad844185199eb73a82ba12
np0805 <nickypratama17@yahoo.com>
2020-04-28 21:06:28 +0700

    redesign main.go

--- src/main.go
+++ src/main.go
@@ -7,7 +7,8 @@ import (
 	"./crawler"
 )
 
-// Crawl crawl a given url as its base url, returning a mapping of url --> page struct
+// Crawl crawl a given url as its base url,
+// returning a mapping of url --> page struct
 func Crawl(baseURL string) map[string]*crawler.Page {
 	pagesMap := make(map[string]*crawler.Page)
 	basePage := crawler.Page{
@@ -25,25 +26,28 @@ func Crawl(baseURL string) map[string]*crawler.Page {
 	basePage.ExtractSize()
 	basePage.ExtractLinks()
 	pagesMap[baseURL] = &basePage
+	basePage.MakeChildren(&pagesMap)
 
 	return pagesMap
 }
 
 func main() {
 	const baseURL = "https://www.cse.ust.hk/"
-	fmt.Println(time.Now())
+	fmt.Println(time.Now()) // buat ngecek dia brp lama runnya
 
-	pagesMap := Crawl(baseURL)
-	basePage := pagesMap[baseURL]
-
-	basePage.WriteIndexed(&pagesMap)
-
-	// // contoh cara ngeindex dari map
-	// another := pagesMap["http://epublish.ust.hk/cgi-bin/eng/story.php?id=96&catid=97&keycode=88b7aae0ae45ddb0e6e000ee2682721a&token=17b43a00aeb0f8f8f08df16ae664909f"]
-	// fmt.Println(another.GetParentURL())
-	// fmt.Println(pagesMap["http://epublish.ust.hk/cgi-bin/eng/story.php?id=96&catid=97&keycode=88b7aae0ae45ddb0e6e000ee2682721a&token=17b43a00aeb0f8f8f08df16ae664909f"])
-	// fmt.Println(len(pagesMap))
+	pagesMap := Crawl(baseURL) // get the mapping of url --> page struct
 
 	fmt.Println(time.Now())
 
+	// contoh cara ngambil page dari map
+	for _, page := range pagesMap {
+		fmt.Println(page.GetTitle())
+		fmt.Println(page.GetKeywords())
+		break
+	}
+	/*
+		contoh cara lain buat ngambil page dari map
+		another := pagesMap["http://epublish.ust.hk/cgi-bin/eng/story.php?id=96&catid=97&keycode=88b7aae0ae45ddb0e6e000ee2682721a&token=17b43a00aeb0f8f8f08df16ae664909f"]
+		fmt.Println(another.GetTitle())
+	*/
 }
