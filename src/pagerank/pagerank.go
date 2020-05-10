416598fc745e3c19a5ce732698d316698ce7980d
np0805 <nickypratama17@yahoo.com>
2020-05-10 12:57:24 +0700

    add stoping criteria in page rank

--- src/pagerank/pagerank.go
+++ src/pagerank/pagerank.go
@@ -1,12 +1,18 @@
 package pagerank
 
 import (
+	"math"
+
 	"../crawler"
 )
 
 // CalculatePageRank given a damping factor and map of pages, calculate the ranks recursively
 func CalculatePageRank(d float64, pages *map[string]*crawler.Page) {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 1000; i++ {
+		// converge := make([]bool, 0)
+		// for i := 0; i < len(*pages); i++ {
+		// 	converge[i] = false
+		// }
 		for _, page := range *pages {
 			var myRank float64 = 1 - d // value for page rank
 			var runningSum float64 = 0 // running sum for probablity from its parents
@@ -19,6 +25,10 @@ func CalculatePageRank(d float64, pages *map[string]*crawler.Page) {
 				}
 			}
 			myRank = myRank + d*runningSum
+			difference := myRank - page.GetPageRank()
+			if math.Abs(difference) < 0.00000000000005 { // showing signs of converging
+				break
+			}
 			page.SetRank(myRank)
 		}
 	}
